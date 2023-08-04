5bf9e2f1d7353dc23ad5484a51555aa00b60dd89
Solano15 <110858396+Solano15@users.noreply.github.com>
2023-08-04 15:44:32 -0600

    Update tarea_1_example_solution.py
    Código sin errores de flake8

--- Tarea 1/tarea_1_example_solution.py
+++ Tarea 1/tarea_1_example_solution.py
@@ -2,31 +2,32 @@
 def separa_letras(string):
     # Comprobamos si el parámetro no es un string
     if not isinstance(string, str):
-        return -100,None, None
+        return -100, None, None
 
     # Comprobamos si el string está vacío
     if not string.strip():
-        return -300,None, None
+        return -300, None, None
 
     # Comprobamos si el string solo contiene letras del abecedario
     if not string.isalpha():
-        return -200,None, None
+        return -200, None, None
 
     # Separamos el string en dos: uno con minúsculas y otro con mayúsculas
     minusculas = ''.join(char for char in string if char.islower())
     mayusculas = ''.join(char for char in string if char.isupper())
 
-    return 0,mayusculas,minusculas
-    
+    return 0, mayusculas, minusculas
+
+
 def potencia_manual(base, potencia):
     # Verificamos que base y exponente no sean strings
     if isinstance(base, str) or isinstance(potencia, str):
-        return -400,None
+        return -400, None
 
     # Caso especial: exponente es cero, la potencia es siempre 1
     if potencia == 0:
-        return 0,1
-    
+        return 0, 1
+
     # Inicializamos el resultado en 1, ya que cualquier número elevado a 0 es 1
     resultado = 1
 
@@ -37,4 +38,4 @@ def potencia_manual(base, potencia):
             suma += resultado
         resultado = suma
 
-    return 0,resultado
+    return 0, resultado
